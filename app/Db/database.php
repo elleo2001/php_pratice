2076d2529b0ec11edd66c2e9a2397adfd2a81bff
leonardo <leonardosantana28@yahoo.com.br>
2022-07-14 00:39:35 -0800

    first commit

---
+++ app/Db/database.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Db;
+
+use Exception;
+use PDO;
+use PDOException;
+
+class Database{
+
+    /**
+     * Host de conexão com o banco de dados
+     * @var string
+     */
+    const HOST = 'localhost';
+
+    /**
+     * Nome do banco de dados
+     * @var string 
+     */
+    const NAME = 'wdev_vagas';
+
+    /**
+     * Usuário do banco
+     * @var string
+     */
+    const USER = 'root';
+
+    /**
+     * Senha de acesso ao banco de dados
+     * @var string
+     */
+    const PASS = '';
+
+    /**
+     * Nome da tabela a ser manipulada
+     * @var string
+     */
+    private $table;
+
+    /**
+     * Instancia de conexão com o banco de dados
+     * @var PDO
+     */
+    private $connection;
+
+    /**
+     * Define a tabela e instancia a conexão
+     * @param string
+     */
+    public function __construct($table = null) {
+        $this->table = $table;
+        $this->setConnection();
+    }
+
+    /**
+     * Método responsável por criar uma conexão com o banco de dados
+     */
+    private function setConnection(){
+        try{
+            $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::NAME,self::USER,self::PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            die('ERROR: '.$e->getMessage()); 
+        }
+    }
+    
+    /**
+     * Método responsável por inserir dados no banco
+     * @param string $values [ field => value ]
+     * @return array
+     * @return PDOStatement
+     */
+    
+    public function execute($query,$params = []) {
+        try{
+            $statement = $this->connection->prepare($query);
+            $statement->execute($params);
+            return $statement;
+        }catch(PDOException $e){
+            die('ERROR: '.$e->getMessage());
+        }
+    }
+
+    /**
+     * @param array $values [ field => value ]
+     * @return integer
+     */
+    public function insert($values) {
+        $fields = array_keys($values);
+        $binds = array_pad([],count($fields),'?');
+
+        //MONTA A QUERY
+        $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+
+        //EXECUTA O INSERT
+        $this->execute($query,array_values($values));
+        
+        //RETORNA O ID INSERIDO
+        return $this->connection->lastInsertId();
+    }
+}
---
+++ includes/listagem.php
@@ -0,0 +1,24 @@
+<main>
+
+    <section>
+        <a href="cadastrar.php">
+            <button class="btn btn-success">Nova vaga</button> 
+        </a>
+    </section>
+
+    <table class="table">
+
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Descrição</th>
+                <th>Status</th>
+                <th>ID</th>
+                <th>ID</th>
+        
+
+    </table>
+
+
+</main>
